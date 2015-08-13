db0e39af0460ab559e8430c4fdca039f6d534285
David Burles <dburles@gmail.com>
2015-08-13 10:55:55 +1000

    bring up to date

---
+++ README.md
@@ -0,0 +1,15 @@
+# Meteor Factory Client
+
+Add-on package for Factory that allows unrestricted client-side creation of factory data.
+
+## Installation
+
+```sh
+$ meteor add dburles:factory-client
+```
+
+## License 
+
+MIT. (c) Percolate Studio
+
+factory-client was developed as part of the [Verso](http://versoapp.com) project.
--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
-  summary: 'Add-on package for Factory that allows unrestricted client-side creation of factory data'
+  name: 'dburles:factory-client',
+  version: '0.1.4',
+  summary: 'Add-on package for Factory that allows unrestricted client-side creation of factory data',
+  git: 'https://github.com/percolatestudio/meteor-factory-client.git',
+  documentation: 'README.md'
 });
 
-Package.on_use(function(api) {
-  api.use('factory');
-  api.add_files('lib/factory-client.js', ['client', 'server']);
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+  api.use('dburles:factory@0.3.10');
+  api.add_files('lib/factory-client.js');
   api.export('flushWrites', 'client');
-});
-
-Package.on_test(function(api) {
-  api.use('factory-client');
 });
